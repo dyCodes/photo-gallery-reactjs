ce41625c89f9823d3a5ca230a3d8f2470908425b
dyCodes <yusufdaudu51@gmail.com>
2022-04-15 19:28:36 +0100

    Implement API using axios

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,25 +2,29 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Typography from "@mui/material/Typography";
 
-const Gallery = ({ photos, term }) => {
+const Gallery = ({ photoList, term }) => {
+
   return (
-    <>
+    <div className="gallery">
       <Typography variant="h5" component="h2" mb={3} textTransform="capitalize" sx={{ opacity: .8 }}>
         {term}
       </Typography>
 
-      <ImageList cols={4} gap={6}>
-        {photos.map((item) => (
+      {photoList.length ? (<ImageList cols={4} gap={6}>
+        {photoList.map((item) => (
           <ImageListItem key={item.id}>
             <img
-              src={item.urls.regular}
+              src={item.urls.small}
               alt={item.description}
               loading="lazy"
             />
           </ImageListItem>
         ))}
       </ImageList>
-    </>
+      ) : (
+        <Typography variant="h6" sx={{ color: 'red', m: '85px 0' }}>No Photo found!</Typography>
+      )}
+    </div>
   );
 };
 
