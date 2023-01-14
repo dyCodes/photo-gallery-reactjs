e23aa610a3418660e52cd5155b1026d851fb2495
dyCodes <yusufdaudu51@gmail.com>
2023-01-14 15:34:40 +0100

    fix: Update link href value

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,15 @@ import ButtonsTab from "./ButtonsTab";
 import SearchForm from "./SearchForm";
 
 const Header = ({ searchTerm, setSearchTerm, term, getPhotos }) => {
-    return (
-        <header>
-            <Typography variant="h4" className="_title" component="h1" mb={3}>
-                <a href="">Photo<span>Gallery</span></a>
-            </Typography>
-            <SearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} getPhotos={getPhotos} />
-            <ButtonsTab getPhotos={getPhotos} term={term} />
-        </header>
-    )
+  return (
+    <header>
+      <Typography variant="h4" className="_title" component="h1" mb={3}>
+        <a href="/">Photo<span>Gallery</span></a>
+      </Typography>
+      <SearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} getPhotos={getPhotos} />
+      <ButtonsTab getPhotos={getPhotos} term={term} />
+    </header>
+  )
 }
 
 export default Header
